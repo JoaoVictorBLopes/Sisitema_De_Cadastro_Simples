31619d8a189785fda7809b50d22b399a98f36ff7
Joao Victor Barboza Lopes <lopestitobarbosa@gmail.com>
2025-03-21 13:24:07 -0300

    Criação da interface para acesso aos metodos CRUD no controller

--- Back-end/projeto/src/main/java/br/com/criandoapi/projeto/controller/usuarioController.java
+++ Back-end/projeto/src/main/java/br/com/criandoapi/projeto/controller/usuarioController.java
@@ -1,14 +1,19 @@
 package br.com.criandoapi.projeto.controller;
+import br.com.criandoapi.projeto.DAO.IUsuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class usuarioController {
+public class usuarioController{
+
+    @Autowired
+    private IUsuario dao;
 
     @GetMapping("/usuarios")
     public String texto () {
-        return "Acessando a api";
+        return "teste";
     }
 
 
