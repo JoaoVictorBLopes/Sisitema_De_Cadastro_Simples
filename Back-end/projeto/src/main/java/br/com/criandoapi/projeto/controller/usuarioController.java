ef822dc4cc8b6d1c8cff7b25a3fc65d3027cd1f2
Joao Victor Barboza Lopes <lopestitobarbosa@gmail.com>
2025-03-23 17:40:02 -0300

    Adicionado metodo put e delete no controller do backend

--- Back-end/projeto/src/main/java/br/com/criandoapi/projeto/controller/usuarioController.java
+++ Back-end/projeto/src/main/java/br/com/criandoapi/projeto/controller/usuarioController.java
@@ -2,21 +2,43 @@ package br.com.criandoapi.projeto.controller;
 import br.com.criandoapi.projeto.DAO.IUsuario;
 import br.com.criandoapi.projeto.model.usuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/usuarios")
+
 public class usuarioController{
 
     @Autowired
     private IUsuario dao;
 
-    @GetMapping("/usuarios")
+    @GetMapping
     public List<usuario> listaUsuarios () {
         return (List<usuario>) dao.findAll();
     }
+
+    @PostMapping
+    public usuario criarUsuario (@RequestBody usuario usuario) {
+       usuario usuarioNovo = dao.save(usuario);
+       return usuarioNovo;
+    }
+
+    @PutMapping
+    public usuario editarUsuario (@RequestBody usuario usuario) {
+        usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
+
+    @DeleteMapping("/{id}")
+    public Optional<usuario> excluirUsuario (@PathVariable int id) {
+        Optional<usuario> usuario = dao.findById(id);
+        dao.deleteById(id);
+        return usuario;
+    }
+
+
 
 }
